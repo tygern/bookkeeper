f1798242878f3a294d839413c72834bfd9ae08d7
Tyson Gern <tyson@gern.org>
2021-02-09 19:24:46 -0700

    Move frontend to component

---
+++ components/frontend/build.gradle.kts
@@ -0,0 +1,30 @@
+import org.gradle.api.tasks.Exec
+
+tasks.register<Exec>("clean") {
+    group = "NPM"
+    description = "Clean public/build"
+
+    commandLine("npm", "run", "clean")
+}
+
+tasks.register<Exec>("test") {
+    group = "NPM"
+    description = "Run tests"
+    mustRunAfter("assemble")
+
+    commandLine("npm", "test")
+}
+
+tasks.register<Exec>("assemble") {
+    group = "NPM"
+    description = "Assemble into public/build"
+
+    commandLine("npm", "run", "assemble")
+}
+
+tasks.register("build") {
+    group = "NPM"
+    description = "Build into public/build"
+
+    dependsOn("assemble", "test")
+}
